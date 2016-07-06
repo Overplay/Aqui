4271067819fd6d2810563f63a5b8fffeb35db5c3
JewsOfHazard <logan.saso2016@gmail.com>
2016-07-06 11:45:04 -0700

    Added an orange color to tables that are ready

--- opp/io.ourglass.waitinglist/app/tv/app/components/WaitingListController.js
+++ opp/io.ourglass.waitinglist/app/tv/app/components/WaitingListController.js
@@ -13,54 +13,66 @@
 app.controller('waitingListController', ['$scope', 'optvModel', '$log', '$timeout', function ($scope, optvModel, $log, $timeout) {
 
 	//$scope.parties = function() { return optvModel.model.parties; };
-
+	
 	var parties = [
 		{
 			name: "Logan Saso",
-			partySize: 12
+			partySize: 12,
+			tableReady: false
 		},
 		{
 			name: "Noah Saso",
-			partySize: 1
-		},{
+			partySize: 1,
+			tableReady: true
+		}, {
 			name: "Logan Saso",
-			partySize: 12
+			partySize: 12,
+			tableReady: false
 		},
 		{
 			name: "Christopher Saso",
-			partySize: 1
-		},{
+			partySize: 1,
+			tableReady: false
+		}, {
 			name: "Arabella Appleseed Saso",
-			partySize: 12
+			partySize: 12,
+			tableReady: false
 		},
 		{
 			name: "Noah Saso",
-			partySize: 1
-		},{
+			partySize: 1,
+			tableReady: false
+		}, {
 			name: "Logan Saso",
-			partySize: 12
+			partySize: 12,
+			tableReady: false
 		},
 		{
 			name: "Noah Saso",
-			partySize: 1
-		},{
+			partySize: 1,
+			tableReady: true
+		}, {
 			name: "Logan Saso",
-			partySize: 12
+			partySize: 12,
+			tableReady: false
 		},
 		{
 			name: "Noah Saso",
-			partySize: 1
-		},{
+			partySize: 1,
+			tableReady: false
+		}, {
 			name: "Logan Saso",
-			partySize: 12
+			partySize: 12,
+			tableReady: false
 		},
 		{
 			name: "Noah Saso",
-			partySize: 1
+			partySize: 1,
+			tableReady: false
 		}
 	];
 
-	$scope.parties = function(){
+	$scope.parties = function () {
 		return parties;
 	};
 
@@ -107,7 +119,7 @@ app.directive('topScroller', function ($timeout) {
 				})
 			}
 
-			var i, dx, delay = 50, top;
+			var i, dx, delay = 35, top;
 
 			function loop() {
 				if (i >= dx) {
@@ -137,6 +149,71 @@ app.directive('topScroller', function ($timeout) {
 		}
 	}
 });
+
+
+app.directive('topScrollerSteps', function ($timeout, $interval) {
+	return {
+		restrict: 'E',
+		scope: {
+			parties: '='
+		},
+		templateUrl: 'app/components/directives/topscroller.template.html',
+		link: function (scope, elem, attrs) {
+			scope.topPos = {};
+
+			function loadHeight() {
+				return $timeout(function () {
+					return document.getElementById('party-container').offsetHeight;
+				})
+			}
+
+			var i, dx, delay = 500, topOfContent, heightOfOne = 50;
+
+
+			function outerLoop() {
+
+				if (i >= dx) {
+					console.log('Done!');
+					beginScroll();
+					return;
+				}
+				i += heightOfOne;
+				loop();
+			}
+
+
+			function loop() {
+
+				var j = 0;
+				var interval = $interval(function () {
+					j++;
+					scope.topPos.top = --topOfContent + 'px';
+					if (j >= heightOfOne) {
+						$interval.cancel(interval);
+						$timeout(outerLoop, delay);
+					}
+				}, 20);
+
+			}
+
+			function beginScroll() {
+				console.log('Beginning scroll...');
+				i = 1;
+				loadHeight().then(function (heightOfPartyContainer) {
+					topOfContent = 300;
+					dx = heightOfPartyContainer + topOfContent;
+					scope.topPos.top = topOfContent + 'px';
+					console.log('Scroll starting. Got height:', heightOfPartyContainer, 'and window height:', window.innerHeight, 'and dx:', dx);
+					outerLoop();
+				});
+			}
+
+			beginScroll();
+
+		}
+	}
+});
+
 
 app.filter('nameMaximum', function () {
 
