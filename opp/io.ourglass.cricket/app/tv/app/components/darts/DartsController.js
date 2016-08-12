2de41f07894e4cad00020fa69f25baa8efe56aeb
Ethan Adams <theride40@gmail.com>
2016-08-12 16:26:15 -0700

    fixed darts app

--- opp/io.ourglass.cricket/app/tv/app/components/darts/DartsController.js
+++ opp/io.ourglass.cricket/app/tv/app/components/darts/DartsController.js
@@ -67,87 +67,81 @@ app.controller("dartsController",
 
         });
 
-        function updateLocalScore() {
-            console.log(_remoteScore);
-        }
 
         function modelUpdate(data) {
             $log.info(logLead() + " got a model update: " + angular.toJson(data));
             _remoteScore = data;
-            updateLocalScore();
+            $scope.$apply(function(){
+                //game logic
+                $scope.score.red = 0;
+                $scope.score.blue = 0;
 
-            //game logic
-            $scope.score.red = 0;
-            $scope.score.blue = 0;
-
-            $scope.rows = [
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                },
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                },
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                },
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                },
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                },
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                },
-                {
-                    p1: {numHits: 0, score: 0},
-                    p2: {numHits: 0, score: 0},
-                    closed: false
-                }
-            ];
-
-            _remoteScore.turns.forEach(function(turn){
-                for (var i = 0; i < turn.player1.board.length; i++) {
-                    var redHits = 0, blueHits = 0;
-                    $scope.rows[i].p1.numHits += turn.player1.board[i].darts.length;
-                    redHits += turn.player1.board[i].darts.length;
-
-                    if ($scope.rows[i].p1.numHits >= 3 && $scope.rows[i].p2.numHits >= 3) {
-                        $scope.rows[i].closed = true;
+                $scope.rows = [
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
+                    },
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
+                    },
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
+                    },
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
+                    },
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
+                    },
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
+                    },
+                    {
+                        p1: {numHits: 0, score: 0},
+                        p2: {numHits: 0, score: 0},
+                        closed: false
                     }
-                    if (!$scope.rows[i].closed && $scope.rows[i].p1.numHits > 3) {
-                        $scope.rows[i].p1.score += scoreReference[i] * (redHits < 3 ? 0 : redHits - 3);
-                    }
-                    $scope.rows[i].p2.numHits += turn.player2.board[i].darts.length;
-                    blueHits += turn.player2.board[i].darts.length;
-                    if ($scope.rows[i].p1.numHits >= 3 && $scope.rows[i].p2.numHits >= 3) {
-                        $scope.rows[i].closed = true;
-                    }
-                    if (!$scope.rows[i].closed && $scope.rows[i].p2.numHits > 3) {
-                        $scope.rows[i].p2.score += scoreReference[i] * (blueHits < 3 ? 0 : blueHits - 3);
-                    }
-                }
-            });
+                ];
 
-            $scope.rows.forEach(function(row){
-                $scope.score.red += row.p1.score;
-                $scope.score.blue += row.p2.score;
+                _remoteScore.turns.forEach(function(turn){
+                    for (var i = 0; i < turn.player1.board.length; i++) {
+                        var redHits = 0, blueHits = 0;
+                        $scope.rows[i].p1.numHits += turn.player1.board[i].darts.length;
+                        redHits += turn.player1.board[i].darts.length;
+
+                        if ($scope.rows[i].p1.numHits >= 3 && $scope.rows[i].p2.numHits >= 3) {
+                            $scope.rows[i].closed = true;
+                        }
+                        if (!$scope.rows[i].closed && $scope.rows[i].p1.numHits > 3) {
+                            $scope.rows[i].p1.score += scoreReference[i] * (redHits < 3 ? 0 : redHits - 3);
+                        }
+                        $scope.rows[i].p2.numHits += turn.player2.board[i].darts.length;
+                        blueHits += turn.player2.board[i].darts.length;
+                        if ($scope.rows[i].p1.numHits >= 3 && $scope.rows[i].p2.numHits >= 3) {
+                            $scope.rows[i].closed = true;
+                        }
+                        if (!$scope.rows[i].closed && $scope.rows[i].p2.numHits > 3) {
+                            $scope.rows[i].p2.score += scoreReference[i] * (blueHits < 3 ? 0 : blueHits - 3);
+                        }
+                    }
+                });
+
+                $scope.rows.forEach(function(row){
+                    $scope.score.red += row.p1.score;
+                    $scope.score.blue += row.p2.score;
+                })
             })
-
-            $log.debug(logLead() + "Model update callback...")
-
         }
 
         function inboundMessage(msg) {
