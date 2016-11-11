f4a00a3ae741b6383d5769de77657efaf16050a8
Mitch Kahn <mitch@ourglass.io>
2016-11-11 14:36:42 -0800

    Fixed shuffleboard data corruption (race) issue.

--- opp/io.ourglass.shuffleboard/app/control/app/components/shuffcon/ShuffconController.js
+++ opp/io.ourglass.shuffleboard/app/control/app/components/shuffcon/ShuffconController.js
@@ -14,9 +14,9 @@ app.controller( "shuffconController",
 
             ogControllerModel.init( {
                 appName:         "io.ourglass.shuffleboard",
-                dataCallback:    function(data){
-                    $log.debug("New data: "+data);
-                }
+                // dataCallback:    function(data){
+                //     $log.debug("New data: "+data);
+                // }
             } );
 
         }
