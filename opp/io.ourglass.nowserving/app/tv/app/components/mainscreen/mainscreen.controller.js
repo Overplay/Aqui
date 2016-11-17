1909259d9e152e97c236886f721592979c134a9f
Mitch Kahn <mitch@ourglass.io>
2016-11-17 09:50:53 -0800

    Scaffolding done for NowServing.

---
+++ opp/io.ourglass.nowserving/app/tv/app/components/mainscreen/mainscreen.controller.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mkahn on 11/17/16.
+ */
+
+app.controller("mainScreenController", function($scope, $log){
+
+    $log.debug( "mainScreenController has loaded");
+
+});
---
+++ opp/io.ourglass.nowserving/app/tv/app/ogNowServingApp.module.js
@@ -0,0 +1,5 @@
+/**
+ * Created by mkahn on 11/17/16.
+ */
+
+var app = angular.module("ogNowServingApp",[]);
