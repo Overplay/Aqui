626571e8057075431f11f26bbf0435cf02ac7bcf
erikphillips <erik.phillips@icloud.com>
2017-01-18 18:54:53 -0800

    initial commit for sqaures app

--- opp/io.ourglass.squares/app/control/app/components/controlscreen/controlscreen.controller.js
+++ opp/io.ourglass.squares/app/control/app/components/controlscreen/controlscreen.controller.js
@@ -2,8 +2,8 @@
  * Created by Erik Phillips on 1/18/17.
  */
 
-app.controller( "ogSquaresController", function ( $scope, $log, ogControllerModel ) {
+app.controller( "ogSquaresController", function ( $scope, $log ) {
 
     $log.debug( "loaded ogSquaresController" );
 
-} );
+});
