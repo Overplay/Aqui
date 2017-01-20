2cc5d4198096bdaa2f613e34e08cf9fd076165dc
erikphillips <erik.phillips@icloud.com>
2017-01-20 12:55:06 -0800

    updates to the tv app

--- opp/io.ourglass.squares/app/tv/app/components/mainscreen/mainscreen.controller.js
+++ opp/io.ourglass.squares/app/tv/app/components/mainscreen/mainscreen.controller.js
@@ -2,7 +2,7 @@
  * Created by Erik Phillips on 1/18/17.
  */
 
-app.controller("mainScreenController", function($scope, $log, ogTVModel, $interval){
+app.controller("mainScreenController", function($scope, $log ){
 
     $log.debug( "mainScreenController has loaded");
 
