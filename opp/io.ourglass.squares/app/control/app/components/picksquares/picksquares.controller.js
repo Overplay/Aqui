1e1efd76859e9fe0dc68b002bcd9ab24569a217e
erikphillips <erik.phillips@icloud.com>
2017-01-20 14:51:00 -0800

    updates to css and logic added

--- opp/io.ourglass.squares/app/control/app/components/picksquares/picksquares.controller.js
+++ opp/io.ourglass.squares/app/control/app/components/picksquares/picksquares.controller.js
@@ -2,26 +2,81 @@
  * Created by mkahn on 1/19/17.
  */
 
-app.controller("pickSquaresController", function($scope, uibHelper, $log){
+app.controller("pickSquaresController", function($scope, $rootScope, $state, uibHelper, $log){
+
+    var demoState = true;
+
 
     $log.debug("loading pickSquaresController");
 
-
-    $scope.rows = [ 0, 1,2,3,4,5,6,7,8,9 ];
+    $scope.rows = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
     $scope.cols = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
     
     $scope.grid = [];
-    
-    for (var col=0; col<10; col++){
-        var row = [];
-        for (var r=0; r<10; r++){
-            row.push(false);
+    $scope.numPicked = 0;
+
+    if (!$rootScope.currentUser && demoState) {
+        $rootScope.currentUser = {
+            picksAllowed: 3,
+            fullname: 'Erik Phillips',
+            initials: 'DEMO',
+            email: 'erik.phillips@icloud.com'
         }
-        $scope.grid.push(row);
     }
-    
+
+    if (!$rootScope.currentUser) {
+        $log.debug("no current user found");
+        $state.go("register");
+    }
+
+
+    var initializeGrid = function () {
+        for (var col=0; col<10; col++){
+            var row = [];
+            for (var r=0; r<10; r++){
+                row.push(false);
+            }
+            $scope.grid.push(row);
+        }
+    };
+
+    initializeGrid();
+
     $scope.clicked = function(row, col){
+        if (!$scope.grid[row][col] && $scope.numPicked >= $rootScope.currentUser.picksAllowed) {
+            alert("No more picks allowed.");
+            return;
+        }
+
         $scope.grid[row][col] = !$scope.grid[ row ][ col ];
-    }
+
+        if ($scope.grid[row][col]) {
+            $scope.numPicked += 1;
+        } else {
+            $scope.numPicked -= 1;
+        }
+    };
+
+    $scope.clearSelections = function () {
+        $log.debug("clearing all selections");
+
+        // uibHelper.confirmModal("Clear All Selections?", "Would you like to clear all your current selections?", true)
+        //     .then(function(){
+        //         $scope.grid = [];
+        //         initializeGrid();
+        //     });
+
+        $scope.grid = [];
+        $scope.numPicked = 0;
+        initializeGrid();
+    };
+
+    $scope.submitSelection = function () {
+        if ($scope.numPicked != $rootScope.currentUser.picksAllowed) {
+            alert("Please select " + $rootScope.currentUser.picksAllowed + " squares.");
+        }
+
+        $log.debug("submit now!")
+    };
 
 });
--- opp/io.ourglass.squares/app/control/app/components/register/register.controller.js
+++ opp/io.ourglass.squares/app/control/app/components/register/register.controller.js
@@ -2,11 +2,38 @@
  * Created by mkahn on 1/19/17.
  */
 
-app.controller("registerController", function($scope, uibHelper, $log){
+app.controller("registerController", function($scope, $rootScope, uibHelper, $log, $state){
 
     $log.debug("loading registerController");
 
+    $scope.clearForm = function () {
+        $scope.fullname = undefined;
+        $scope.email = undefined;
+        $scope.picks = undefined;
+    };
+
+    $scope.clearForm();
+
+    $scope.register = function () {
+        $log.debug("register button clicked");
+
+        if ($scope.picks <= 0 || !$scope.email || !$scope.fullname) {
+            alert("Please complete form");
+            return;
+        }
+
+        var initals = "EP";
+
+        $rootScope.currentUser = {
+            picksAllowed: $scope.picks,
+            fullname: $scope.fullname,
+            email: $scope.email,
+            initials: initals
+        };
 
 
+        $log.debug("changing route");
+        $state.go("picksquares");
+    };
 
 });
