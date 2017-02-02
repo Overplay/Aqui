b7fcf04e31bb3483a0fac00abb896f3b5008d3eb
erikphillips <erik.phillips@icloud.com>
2017-02-01 16:27:10 -0800

    basic squareboard page ready

--- opp/io.ourglass.squares/app/control/app/components/squareboard/squareboard.controller.js
+++ opp/io.ourglass.squares/app/control/app/components/squareboard/squareboard.controller.js
@@ -5,24 +5,20 @@
 app.controller("squareboardController", function($scope, uibHelper, $log, $state,
                $timeout, $interval, toastr, sqGameService, grid) {
 
-
-    $log.debug("sqaurebaord controller loaded");
-
+    $log.debug("squareboard controller loaded");
 
     $scope.grid = grid;
-    $scope.currentScore = {team1: 0, team2: 0};
     $scope.teamNames = {team1: "team1", team2: "team2"};
 
-    var winnerRowCol = {rowIdx: -1, colIdx: -1};
-
     updateScopeGrid();
-    updateCurrentScore();
     updateTeamNames();
     updateScoreMapping();
 
     $scope.emptyArray = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // 11 elements for the ng-repeat
 
     $scope.displayInfo = function(row, col) {
+        if (row < 0 || row > 10 || col < 0 || col > 10) return;
+
         if ($scope.grid[row][col].available) {
             uibHelper.confirmModal("Unpurchased Square", "This square has not been taken.");
             return;
@@ -44,24 +40,10 @@ app.controller("squareboardController", function($scope, uibHelper, $log, $state
     };
 
     $scope.getClassType = function (row, col) {
-        if (row - 1 == winnerRowCol.rowIdx && col - 1 == winnerRowCol.colIdx) return 'winner';
         if (row == 0 && col == 0) return 'empty';
         if (row == 0 || col == 0) return 'header';
-        if (!$scope.grid[row - 1][col - 1].available) return 'taken';
-        return 'free';
+        return 'taken';
     };
-
-    function updateCurrentScore() {
-        // gets the current score of the game
-        sqGameService.getCurrentScore()
-            .then(function( scores ) {
-                $scope.currentScore = scores;
-            })
-            .catch(function () {
-                $log.error("Unexpected rejection getting current scores - using 0-0");
-                $scope.currentScore = {team1: 0, team2: 0};
-            })
-    }
 
     function updateTeamNames() {
         sqGameService.getTeams()
@@ -78,7 +60,6 @@ app.controller("squareboardController", function($scope, uibHelper, $log, $state
         sqGameService.getScoreMap()
             .then( function ( map ) {
                 $scope.scoreMap = map;
-                findWinner();
             })
             .catch( function ( err ) {
                 $log.error( "Unexpected rejection getting score map" );
@@ -88,31 +69,6 @@ app.controller("squareboardController", function($scope, uibHelper, $log, $state
     function updateScopeGrid() {
         $log.debug("update to grid");
         $scope.grid = sqGameService.getRawGrid();
-    }
-
-    function findWinner() {
-        var col = $scope.currentScore.team1 % 10;
-        var row = $scope.currentScore.team2 % 10;
-        var rowMap = $scope.scoreMap.rowScoreMap;
-        var colMap = $scope.scoreMap.colScoreMap;
-
-        for (var r = 0; r < 10; r++)
-            if (rowMap[r] == row)
-                break;
-
-        for (var c = 0; c < 10; c++)
-            if (colMap[c] == col)
-                break;
-
-        winnerRowCol = {rowIdx: r, colIdx: c};
-
-        if ($scope.grid[r][c].available) {
-            $scope.winner = "free square";
-        } else {
-            $scope.winner = $scope.grid[r][c].ownedBy.name;
-        }
-
-        return {rowIdx: r, colIdx: c};
     }
 
 });
